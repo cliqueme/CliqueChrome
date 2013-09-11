20904d48565f982b23dde5cbf8d6d5b6354dac8c
Roman Nikitin <roma@nikitin.me>
2013-09-10 22:25:03 -0300

    rewrite plugin to use it with cliqueme.com

--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,13 @@
-chrome.storage.local.get('appendHtml', function (data) {
-    if (data) {
-        $('body').append(data.appendHtml);
-    }
+$(function(){
+    chrome.storage.local.get(null, function (data) {
+        if (data) {
+            // init window script
+            $.getScript('http://dev.backend.cliqueme.com/inject?site_id='+ data.site_id +'&selector=' + encodeURIComponent(data.selector), function(){
+                console.log(window.cliqueme);
+
+                $.getScript(data.url);
+                $('body').append(data.html);
+            })
+        }
+    });
 });
--- js/popup.js
+++ js/popup.js
@@ -2,15 +2,21 @@ $(function() {
     console.log('hello from content script!');
 
     // init textarea with stored value
-    chrome.storage.local.get('appendHtml', function (data) {
-        if (data){
-            $('#html-to-append').val(data.appendHtml);
-        }
+    chrome.storage.local.get(null, function (data) {
+        console.log('storage', data);
+        $('#txt-html').val(data.html);
+        $('#txt-siteid').val(data.site_id);
+        $('#txt-selector').val(data.selector);
+        $('#txt-url').val(data.url);
     });
 
     // handle submit click
     $('#btn-go').click(function(){
-        set_and_go($('#html-to-append').val());
+
+        set_and_go($('#txt-html').val(),
+            $('#txt-siteid').val(),
+            $('#txt-selector').val(),
+            $('#txt-url').val());
 
         return false;
     });
@@ -18,18 +24,24 @@ $(function() {
     // clear button click
     $('#btn-clear').click(function(){
         // clear textarea
-        $('#html-to-append').val('');
+        $('#txt-html').val('');
+		$('#txt-javascript').val('');
+		$('#txt-url').val('');
+		
         // reload the page
-        set_and_go("");
+        set_and_go('', '', '');
     });
 
 });
 
-function set_and_go(html){
+function set_and_go(html, site_id, selector, url){
     chrome.tabs.getSelected(null, function(tab) {
         // put html into the storage
         chrome.storage.local.set({
-            appendHtml: html
+            html: html,
+			site_id: site_id,
+            selector: selector,
+			url: url
         });
 
         // reload the tab
